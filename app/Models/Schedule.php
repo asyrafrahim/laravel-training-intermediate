df14268e516376bc0508857306bc72e845887a25
Asyraf Rahim <asyrafboyz@gmail.com>
2021-12-03 23:55:12 +0800

    update model relationship, get submitted date

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,13 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
-
+    
+    protected $table = 'articles';
     protected $fillable = [
         'title', 'description', 'trainer', 'user_id', 'attachment'
     ];
-
+    
     public function user()
     {
-        return $this->belongsTo('App\User');
+        return $this->belongsTo(User::class);
     }
+      
+    //define $article->submitted_date
+    public function getSubmittedDateAttribute(){
+        return $this->created_at->format('d/m/Y');
+    }
+    
 }
+
