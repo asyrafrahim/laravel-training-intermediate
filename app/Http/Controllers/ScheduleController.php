2b33acab2660c466bbe9db9d7125a23b217df473
Asyraf Rahim <asyrafboyz@gmail.com>
2021-12-04 10:11:05 +0800

    update ScheduleController

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $schedules = Schedule::all();
+        return view('schedules.index', compact('schedules'));
+    }
+
+    public function create()
+    {
+        return view('schedules.create');
+    }
+
+    public function store(Request $request)
+    {
+        $schedule = new Schedule();
+        $schedule->title = $request->title;
+        $schedule->description = $request->description;
+        $schedule->user_id = auth()->user()->id;
+        $schedule->save();
+
+        return redirect()->route('schedule:index');
+    }
 }
