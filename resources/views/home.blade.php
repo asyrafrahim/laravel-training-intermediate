fb008638ae924d586ce6f41343092af320b3e46f
Asyraf Rahim <asyrafboyz@gmail.com>
2021-12-07 13:01:37 +0800

    display login status role in home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,28 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
+                
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
-
+                    
+                    @can('isAdmin')
+                    <div class="btn btn-success btn-lg">
+                        You have Admin Access
+                    </div>
+                    @elsecan('isManager')
+                    <div class="btn btn-primary btn-lg">
+                        You have Manager Access
+                    </div>
+                    @else
+                    <div class="btn btn-info btn-lg">
+                        You have User Access
+                    </div>
+                    @endcan
+                    
                     {{ __('You are logged in!') }}
                 </div>
             </div>
