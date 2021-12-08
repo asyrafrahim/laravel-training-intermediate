94d445ad6fffa6ff3696659adfa6a9cef8a1ab35
Asyraf Rahim <asyrafboyz@gmail.com>
2021-12-08 13:01:59 +0800

    create schdule email blade view

---
+++ resources/views/email/schedule-created.blade.php
@@ -0,0 +1,8 @@
+<h1>Congratulations</h1>
+
+<p> Your schedule has been created using Mailable class</p>
+
+<p>Schedule title : {{ $schedule->title }}</p>
+<p>Schedule description : {{ $schedule->description }}</p>
+
+<p>Thank you</p>
